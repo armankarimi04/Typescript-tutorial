3e88e94ab8fddd7fe7cf4ffd74913d4a0eba5961
armk7 <armankarimy7@gmail.com>
2025-12-11 15:34:50 +0330

    6.3 interfaces vs abstract classes

---
+++ 6.interfaces/6.3.interfaces_vs_abstract_classes.ts
@@ -0,0 +1,17 @@
+// interfaces
+// can contain only method signatures
+// support multiple interface implementation
+// no constructor
+// usage: Designing contracts and structure.
+
+// vs
+
+// abstract classes:
+// can contain implemented methods and abstract methods
+// supports single class inheritance
+// can have constructor for initialization
+// usage: Sharing functionality among related classes.
+
+
+// there are many more...
+// these are just my picks.
